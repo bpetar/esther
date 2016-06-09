5d5d2274c0f46c12651860c0048296d89d50c296
Petar Bajic <petar@pbssolutions.net>
2016-06-09 02:11:45 +0000

    adding item model

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ItemsController extends Controller
+{
+    //
+    public function index()
+    {
+    	return 'get all items';
+    }
+}
---
+++ app/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+    protected $fillable = [
+    	'title',
+    	'description',
+    	'price',
+    	'image'
+    ]
+}
