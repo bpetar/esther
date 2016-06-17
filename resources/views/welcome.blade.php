df6406b970c5e2c8d60a25ecf444bf955594d799
Pedja <lukesevic@gmail.com>
2016-06-17 13:34:43 +0200

    pedjina izmena

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 6</div>
             </div>
         </div>
     </body>
